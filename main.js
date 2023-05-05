d8362ddfe5ac5c4469fb00fa6bcbca87a2865e8f
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-05 09:41:10 +0100

    add 2 buttons

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
 // variabili
 const selectSlider = document.querySelector('.slider');
-const selectButtonDown = document.getElementById('down');
+const selectButtonDown = document.getElementById("down");
+const selectButtonUp = document.getElementById("up");
 const imgArray = ['1', '2', '3', '4', '5'];
 let activeItem = 0;
-
 
 
 for (let i = 0; i < imgArray.length; i++) {
     // inserisco il blocco div e img sul DOM
     selectSlider.innerHTML += `<div class="item"><img src="img/0${imgArray[i]}.webp"></div>`;
-
-    // inserisco la classe active
-    const selectItems = document.querySelectorAll('.item');
-    selectItems[activeItem].classList.add('active');
-
 };
 
 
-// al click inserisci mostra l'immagine
-selectButtonDown.addEventListener('click',
+// inserisco la classe active
+const selectItems = document.querySelectorAll('.item');
+selectItems[activeItem].classList.add('active');
+
+
+// al click 
+selectButtonDown.addEventListener("click",
     function () {
-        console.log("ciaoo");
+        selectItems[activeItem].classList.remove('active');
+
+        activeItem++
+
+        if (activeItem == 0) {
+            activeItem = imgArray.length;
+        }
+
+        selectItems[activeItem].classList.add('active');
     }
 );
 
 
+selectButtonUp.addEventListener("click",
+    function () {
+        selectItems[activeItem].classList.remove('active');
 
+        activeItem--
 
+        if (activeItem == imgArray.length - 1) {
+            activeItem = o;
+        }
 
+        selectItems[activeItem].classList.add('active');
 
-        // // togli la classe active
-        // selectItems[activeItem].classList.remove('active');
-        // // passa al successivo
-        // activeItem += 1;
-        // // aggiungi la classe active al successivo
-        // selectItems[activeItem].classList.add('active');
+    }
+);
+
