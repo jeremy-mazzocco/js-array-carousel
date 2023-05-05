6780f35b07a367ebe34d945b3cd114f4e62d335e
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-05 18:31:29 +0100

    last fixes

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 // variabili
 const selectSlider = document.querySelector('.slider');
-const selectButtonDown = document.getElementById("down");
-const selectButtonUp = document.getElementById("up");
+
+
 const imgArray = ['1', '2', '3', '4', '5'];
 let activeItem = 0;
+// 0 1 2 3 4
+
 
 
 for (let i = 0; i < imgArray.length; i++) {
@@ -17,15 +19,17 @@ const selectItems = document.querySelectorAll('.item');
 selectItems[activeItem].classList.add('active');
 
 
-// al click 
-selectButtonDown.addEventListener("click",
+// al click button Down
+const selectButtonDown = document.getElementById('down');
+selectButtonDown.addEventListener('click',
     function () {
+
         selectItems[activeItem].classList.remove('active');
 
         activeItem++
 
-        if (activeItem == 0) {
-            activeItem = imgArray.length;
+        if (activeItem === imgArray.length) {
+            activeItem = 0;
         }
 
         selectItems[activeItem].classList.add('active');
@@ -33,18 +37,21 @@ selectButtonDown.addEventListener("click",
 );
 
 
+// al click button Up
+const selectButtonUp = document.getElementById("up");
 selectButtonUp.addEventListener("click",
     function () {
         selectItems[activeItem].classList.remove('active');
 
-        activeItem--
 
-        if (activeItem == imgArray.length - 1) {
-            activeItem = o;
+
+        if (activeItem === 0) {
+            activeItem = imgArray.length;
         }
+
+        activeItem--
 
         selectItems[activeItem].classList.add('active');
 
     }
 );
-
